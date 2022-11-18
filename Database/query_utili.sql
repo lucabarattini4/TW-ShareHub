6a4c08eefd1856cd605d55da741cc63d862049c8
Luca Barattini <luca.barattini4@studio.unibo.it>
2022-11-18 12:51:25 +0100

    query utili

--- Database/query_utili.sql
+++ Database/query_utili.sql
@@ -4,4 +4,13 @@ FROM `utente`
 WHERE `utente`.`idUtente` IN (SELECT `amicizia`.`codUtente2`
 FROM `amicizia`, `utente`
 WHERE `utente`.`idUtente` = `amicizia`.`codUtente`
-AND `utente`.`nome` = "pippo");       
+AND `utente`.`nome` = "pippo");  
+
+/*Ricerca delle persone che hanno messo like ad un post*/
+
+/*Ricerca delle persone che hanno salvato un post*/
+
+/*Ricerca dei componenti di una determinata chat*/
+
+/*Ricerca di tutti i post pubblicati dai propri amici*/
+
