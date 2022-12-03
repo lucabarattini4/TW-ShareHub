ce124d02402b8b411d6c1b51f875d01c8725d019
Luca Barattini <luca.barattini4@studio.unibo.it>
2022-12-03 17:03:09 +0100

    fixed some problems in register-form.php

--- template/register-form.php
+++ template/register-form.php
@@ -31,7 +31,7 @@
   ?>
   
 <h2 class="d-flex justify-content-center pt-5">Register</h2>
-    <ul>
+    <!--<ul>
       <li class="d-flex justify-content-center pt-5">
         <label for="nome">Nome:</label>
         <input type="text" placeholder="nome" id="nome" name="nome" required/>
@@ -83,5 +83,59 @@
       <li class="d-flex justify-content-center pt-5">
         <input type="submit" name="submit" value="Invia"/>
       </li>
-    </ul>
+    </ul>-->
+    <fieldset>
+      <legend>Dati personali: </legend>
+        <label for="nome">Nome:</label>
+        <input type="text" placeholder="nome" id="nome" name="nome" required/>
+
+        <label for="cognome">Cognome:</label>
+        <input type="text" placeholder="cognome" id="cognome" name="cognome" required/>
+
+        <br/>
+
+        <label for="datanascita">Data di nascita:</label>
+        <input type="date" id="datanascita" name="datanascita" required/>
+
+        <label for="sesso">Sesso:</label>
+        <input type="text" placeholder="sesso" id="sesso" name="sesso" maxlength=1 required/>
+
+        <br/>
+
+        <label for="prefix">Prefisso telefonico:</label>
+        <input type="text" placeholder="prefix" id="prefix" name="prefix" minlength=3 maxlength=3 required/>
+
+        <label for="numero">Numero di telefono:</label>
+        <input type="text" placeholder="numero" id="numero" name="numero" minlength=10 maxlength=10 required>
+
+        <br/>
+
+        <label for="email">Email:</label>
+        <input type="text" placeholder="email" id="email" name="email" required/>
+    </fieldset>
+
+    <fieldset>
+      <legend>Dati ShareHub: </legend>
+
+    <label for="username">Username:</label>
+    <input type="text" placeholder="username" id="username" name="username" required/>
+
+    <br/>
+
+    <label for="psw">Password:</label>
+    <input type="password" placeholder="password" id="psw" name="psw" required/>
+
+    <label for="psw2">Conferma password:</label>
+    <input type="password" placeholder="conferma password" id="psw2" name="psw2" required/>
+
+    <br/>
+
+    <label for="fileToUpload">Immagine del profilo:</label>
+    <input type="file" id="fileToUpload" name="fileToUpload" required/>
+    <button id="rmv" type="button">Remove file</button>
+
+    <div id="img-preview" class="d-flex justify-content-center pt-5"></div>
+    </fieldset>
+
+    <input type="submit" name="submit" value="Invia"/>
 </form>
